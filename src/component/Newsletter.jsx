cd82a4ba06a37e05784b3a2da32934efd9783057
keerthana11-2002 <jkkeerthana10@gmail.com>
2025-01-06 21:18:38 +0530

    commit

--- src/component/Newsletter.jsx
+++ src/component/Newsletter.jsx
@@ -31,8 +31,8 @@ const Newsletter = () => {
   };
 
   return (
-    <div className="lg:mb-[700px] ">
-      <h1 className="text-2xl text-center lg:text-4xl tracking-wider font-extrabold text-gray-800 mb-8 pt-24">
+    <div className="lg:mb-[700px] lg:pt-[50px] ">
+      <h1 className="text-xl text-center  lg:text-4xl tracking-wider font-extrabold text-gray-800 mb-8 pt-24">
         NEWSLETTER
       </h1>
       <div className="flex lg:ml-1  -mb-[550px] -ml-[180px]">
